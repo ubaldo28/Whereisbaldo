d2d580fb355ec49846966ef5013333b90e6d3827
POTUS Watch <potuswatchdaily@gmail.com>
2026-06-04 00:06:46 +0100

    Fix sitemap crash with draft posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
   site: 'https://whereisbaldo.com',
   output: 'static',
-  integrations: [sitemap()],
+  integrations: [sitemap({
+    filter: (page) => !page.includes('/blog/'),
+  })],
 });
